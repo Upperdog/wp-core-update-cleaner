c135bd1aa358385b39cc045a753a15a7609ad272
Emil Gustafsson <emil@upperdog.se>
2018-10-21 21:22:20 +0200

    Add wp-admin/install.php to list of files to remove. Add support for running the plugin on auto core updates.

--- wp-core-update-cleaner.php
+++ wp-core-update-cleaner.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: WP Core Update Cleaner
- * Description: This plugin automatically removes wp-config-sample.php, readme.html and license files, both default and localized versions, when WordPress is updated.
- * Version: 1.0
+ * Description: This plugin automatically removes some files in the root folder, like wp-config-sample.php, readme and license files, when WordPress is manually or automatically updated.
+ * Version: 1.1.0
  * Author: Upperdog
- * Author URI: http://upperdog.com
+ * Author URI: https://upperdog.com
  * Author Email: hello@upperdog.com
  * License: GPL2
  */
@@ -25,13 +25,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/**
- * In search of a solution to our problem, we found the following thread on 
- * StackExchange which pointed us in the right direction. 
- * http://wordpress.stackexchange.com/questions/57612/how-to-prevent-redirect-to-about-after-core-upgrade
- */
-
-if (!defined('ABSPATH')) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -42,10 +36,11 @@ class WPCoreUpdateCleaner {
 	 *
 	 * @since 1.0
 	 */
-
 	function __construct() {
-		add_action('admin_init', array($this, 'admin_init'));
-		add_action('_core_updated_successfully', array($this, 'update_cleaner'), 0, 1);
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( '_core_updated_successfully', array( $this, 'manual_update_cleaner' ), 0, 1 );
+		add_action( 'pre_auto_update', array( $this, 'auto_update_cron_setup' ) );
+		add_action( 'wp_core_update_cleaner_auto_update_cron', array( $this, 'auto_update_cleaner' ) );
 	}
 
 	/**
@@ -53,41 +48,28 @@ class WPCoreUpdateCleaner {
 	 *
 	 * @since 1.0
 	 */
-
 	function admin_init() {
-
-		// Load plugin translations
-		load_plugin_textdomain('wp-core-update-cleaner', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+		load_plugin_textdomain( 'wp-core-update-cleaner', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
-
+	
 	/**
-	 * Performs the update cleaning. 
-	 * 
-	 * This function removes the unwanted files when WordPress is updated. The 
-	 * cleaning is performed on core update and core re-install. The function removes 
-	 * wp-config-sample.php, readme.html and localized versions of the readme and 
-	 * license files. If the files are removed successfully, the plugin outputs 
-	 * a response message to the core update screen letting you know which files 
-	 * that were removed. 
-	 * 
-	 * @since 1.0
-	 * @param string New version of updated WordPress. 
+	 * Schedule single event for cleaning up after auto core updates.
+	 *
+	 * @since 1.1.0
 	 */
-
-	function update_cleaner($new_version) {
-
-		global $pagenow, $action;
-
-		if ('update-core.php' !== $pagenow) {
-			return;
-		}
-
-		if ('do-core-upgrade' !== $action && 'do-core-reinstall' !== $action) {
-			return;
-		}
-
-		// Remove license, readme files 
-		$remove_files = array(
+	function auto_update_cron_setup() {
+		wp_schedule_single_event( time() + 900, 'wp_core_update_cleaner_auto_update_cron' );
+	}
+	
+	/**
+	 * Remove files
+	 *
+	 * @since 1.1.0
+	 */
+	function remove_files( $show_message = false ) {
+		
+		// Define files to remove
+		$files_to_remove = array(
 			'license.txt', 
 			'licens.html', 
 			'licenza.html', 
@@ -101,32 +83,65 @@ class WPCoreUpdateCleaner {
 			'olvasdel.html', 
 			'readme.html', 
 			'readme-ja.html', 
-			'wp-config-sample.php'
+			'wp-config-sample.php',
+			'wp-admin/install.php'
 		);
 
-		foreach ($remove_files as $file) {
-			if (file_exists(ABSPATH . $file)) {
-				if (unlink(ABSPATH . $file)) {
-					show_message(__('Removing', 'wp-core-update-cleaner') . ' ' . $file . '...');
+		foreach ( $files_to_remove as $file ) {
+			if ( file_exists( ABSPATH . $file ) ) {
+				if ( unlink( ABSPATH . $file ) ) {
+					if ( $show_message ) {
+						show_message( __( 'Removing', 'wp-core-update-cleaner' ) . ' ' . $file . '...' );
+					}
 				}
 			}
 		}
+	}
+	
+	/**
+	 * Manual core update cleaner
+	 * 
+	 * @since 1.0
+	 * @since 1.1.0 Rename function. Move actual removal of files to separate function.
+	 * @param string New version of updated WordPress. 
+	 */
+	function manual_update_cleaner( $new_version ) {
+
+		global $pagenow, $action;
+
+		if ( 'update-core.php' !== $pagenow ) {
+			return;
+		}
+
+		if ( 'do-core-upgrade' !== $action && 'do-core-reinstall' !== $action ) {
+			return;
+		}
+
+		// Remove files
+		$this->remove_files( $show_message = true );
 
 		// Load the updated default text localization domain for new strings
 		load_default_textdomain();
 
 		// See do_core_upgrade()
-		show_message(__('WordPress updated successfully') . '.');
-		show_message('<span>' . sprintf(__('Welcome to WordPress %1$s. <a href="%2$s">Learn more</a>.'), $new_version, esc_url(self_admin_url('about.php?updated'))) . '</span>');
+		show_message( __( 'WordPress updated successfully' ) . '.' );
+		show_message( '<span>' . sprintf( __( 'Welcome to WordPress %1$s. <a href="%2$s">Learn more</a>.' ), $new_version, esc_url( self_admin_url( 'about.php?updated' ) ) ) . '</span>' );
     	echo '</div>';
 
 		// Include admin-footer.php and exit
-		include(ABSPATH . 'wp-admin/admin-footer.php');
+		include( ABSPATH . 'wp-admin/admin-footer.php' );
 
 		exit();
 	}
+	
+	/**
+	 * Auto update cleaner
+	 * 
+	 * @since 1.1.0
+	 */
+	function auto_update_cleaner() {
+		$this->remove_files();
+	}
 }
 
-if (is_admin()) {
-	$wp_core_update_cleaner = new WPCoreUpdateCleaner();
-}
+$wp_core_update_cleaner = new WPCoreUpdateCleaner();
